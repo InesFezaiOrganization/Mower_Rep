3b05898564b98fb6745cb6b5f07eea128de27d05
InesFezai <38087855+InesFezai@users.noreply.github.com>
2019-12-03 09:46:20 +0100

    Starting the main function of the script.

---
+++ Mower_Exercice.py
@@ -0,0 +1,12 @@
+def main():
+    path_tondeuse="Tondeuse.txt"
+
+    f_Tondeuse=open(path_tondeuse, "r")
+    f_Tondeuse=f_Tondeuse.readlines()
+   
+    
+
+    
+
+if __name__ == "__main__":
+    main()
